3ca412804d3816cfb09e6d6401586765943be1f9
Maxime <maxime.lecieuxlmf@gmail.com>
2024-12-20 18:30:35 +0100

    Création de la page de présentation + simulation du texte en BDD

--- app/presentation/page.tsx
+++ app/presentation/page.tsx
@@ -1,9 +1,34 @@
+import { Button } from '@/components/ui/design-system/button/Button';
+import { presentationApp } from '@/lib/BDD/presentation'
+import { PresentationType } from '@/lib/types/presentation-types';
 import React from 'react'
+import Image from 'next/image'
+import { Typography } from '@/components/ui/design-system/typographie/Typographie';
 
 export default function page() {
+
+  const presentation = presentationApp.map((pres: PresentationType) => {
+    if (pres) {
+      return (
+        <div className='grid grid-cols-3'>
+          <div>
+            <Image src={pres.img} alt="Logo" width={400} height={400} />
+          </div>
+          <div className='col-span-2'>
+            <Typography variant='body-lg' component='p'>
+              {pres.description}
+            </Typography>
+          </div>
+        </div>
+        
+      );
+    }
+    return null;
+  });
+
   return (
     <div>
-      Hello world
+      {presentation}
     </div>
   )
 }
---
+++ lib/types/presentation-types.tsx
@@ -0,0 +1,4 @@
+export interface PresentationType {
+    img: string
+    description: string
+}
